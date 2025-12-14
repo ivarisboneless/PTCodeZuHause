aa1ffb68c3e1047e7e5edd5c2f5ebb3f808e27e6
ivarisboneless <janstrok97@gmail.com>
2025-12-14 15:04:51 +0100

    Create disconnectEV3.m

---
+++ disconnectEV3.m
@@ -0,0 +1,6 @@
+function lightDisconnectEV3(brickObj)
+% lightDisconnectEV3(brickObj) i think also the command disconnect would wor here ;) 
+
+    % In MATLAB, removing the object is enough to close the connection.
+    clear brickObj
+end
